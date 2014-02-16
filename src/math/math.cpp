008aa5354b1bf5e2b5962abfac3015a1970919ee
Andor Salga <andorsalga@gmail.com>
2014-02-16 12:27:40 -0500

    added math file.

---
+++ src/math/math.cpp
@@ -0,0 +1,13 @@
+/*
+================================================================================
+
+================================================================================
+*/
+ #include "math.h"
+
+const float ryMath::PI 			= 3.14159265358979323846f;
+const float ryMath::TWO_PI		= 2.0f * PI;
+const float ryMath::HALF_PI		= 0.5f * PI;
+const float ryMath::QUARTER_PI	= 0.25f * PI;
+const float ryMath::DEG_TO_RAD	= PI / 180.0f;
+const float ryMath::RAD_TO_DEG	= 180 / PI;
---
+++ src/math/math.h
@@ -0,0 +1,16 @@
+/*
+================================================================================
+ 
+================================================================================
+*/
+#pragma once
+
+class ryMath {
+public:
+	static const float		PI;
+	static const float 		TWO_PI;
+	static const float 		HALF_PI;
+	static const float		QUARTER_PI;
+	static const float		DEG_TO_RAD;
+	static const float		RAD_TO_DEG;
+};
